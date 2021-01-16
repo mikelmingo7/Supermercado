02bb1be2c079c5dac56dc8cb54bf5af227c5b624
mikel andoni mingo <mikelmingo@opendeusto.es>
2021-01-16 18:36:10 +0100

    base de datos trabajador añadida

--- Supermercado/src/clases/Compra.java
+++ Supermercado/src/clases/Compra.java
@@ -3,48 +3,27 @@ import java.util.ArrayList;
 
 public class Compra {
 
-	protected Cliente cliente;
-	protected ArrayList<Producto> productosComprados;
 	protected int precio;
 	protected String fecha;
-	protected Boolean descuento;
 	
-	public Compra(Cliente cliente, ArrayList<Producto> productosComprados, int precio, String fecha,
+	
+	public Compra(Cliente cliente, int precio, String fecha,
 			Boolean descuento) {
 		super();
-		this.cliente = cliente;
-		this.productosComprados = productosComprados;
 		this.precio = precio;
 		this.fecha = fecha;
-		this.descuento = descuento;
+	
 	}
 	
 	public Compra() {
 		super();
-		this.cliente = null;
-		this.productosComprados = null;
 		this.precio = 0;
 		this.fecha = "";
-		this.descuento = false;
 	}
 
-	public Cliente getCliente() {
-		return cliente;
-	}
 
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
 
-	public ArrayList<Producto> getProductosComprados() {
-		return productosComprados;
-	}
-
-	public void setProductosComprados(ArrayList<Producto> productosComprados) {
-		this.productosComprados = productosComprados;
-	}
-
-	public int getPrecio() {
+public int getPrecio() {
 		return precio;
 	}
 
@@ -60,19 +39,13 @@ public class Compra {
 		this.fecha = fecha;
 	}
 
-	public Boolean getDescuento() {
-		return descuento;
-	}
-
-	public void setDescuento(Boolean descuento) {
-		this.descuento = descuento;
-	}
-
 	@Override
 	public String toString() {
-		return "Compra [cliente=" + cliente + ", productosComprados=" + productosComprados + ", precio=" + precio
-				+ ", fecha=" + fecha + ", descuento=" + descuento + "]";
+		return "Compra [precio=" + precio + ", fecha=" + fecha + "]";
 	}
+
+
+	
 	
 
 	
