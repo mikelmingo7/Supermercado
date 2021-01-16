d41ebe10bb79a4ca59d5001a27b38098515733e0
mikel andoni mingo <mikelmingo@opendeusto.es>
2021-01-16 18:55:01 +0100

    cambios de la clase gestión compra para recibir los productos de la base de datos y el dni del cliente especificado

--- Supermercado/src/clases/Compra.java
+++ Supermercado/src/clases/Compra.java
@@ -2,47 +2,83 @@ package clases;
 import java.util.ArrayList;
 
 public class Compra {
-
+	protected ArrayList<Producto> productos;
+	protected String dniCliente;
 	protected int precio;
 	protected String fecha;
 	
 	
-	public Compra(Cliente cliente, int precio, String fecha,
-			Boolean descuento) {
+	
+	
+	public Compra(ArrayList<Producto> productos, String dniCliente, int precio, String fecha) {
 		super();
+		this.productos = productos;
+		this.dniCliente = dniCliente;
 		this.precio = precio;
 		this.fecha = fecha;
-	
 	}
-	
+
+
 	public Compra() {
 		super();
+		this.productos = null;
+		this.dniCliente = "";
 		this.precio = 0;
 		this.fecha = "";
 	}
 
 
+	public ArrayList<Producto> getProductos() {
+		return productos;
+	}
 
-public int getPrecio() {
+
+	public void setProductos(ArrayList<Producto> productos) {
+		this.productos = productos;
+	}
+
+
+	public String getDniCliente() {
+		return dniCliente;
+	}
+
+
+	public void setDniCliente(String dniCliente) {
+		this.dniCliente = dniCliente;
+	}
+
+
+	public int getPrecio() {
 		return precio;
 	}
+
 
 	public void setPrecio(int precio) {
 		this.precio = precio;
 	}
 
+
 	public String getFecha() {
 		return fecha;
 	}
+
 
 	public void setFecha(String fecha) {
 		this.fecha = fecha;
 	}
 
+
 	@Override
 	public String toString() {
-		return "Compra [precio=" + precio + ", fecha=" + fecha + "]";
+		return "Compra [productos=" + productos + ", dniCliente=" + dniCliente + ", precio=" + precio + ", fecha="
+				+ fecha + "]";
 	}
+	
+
+
+
+
+	
 
 
 	
