6e421eefb6fcaf65d72d237e0bd3382b80178772
markelmorales15 <72606696+markelmorales15@users.noreply.github.com>
2021-01-17 20:52:43 +0100

    ventana trabajador arreglada y mejorada

--- Supermercado/src/inicios/VentanaLoginTrabajador.java
+++ Supermercado/src/inicios/VentanaLoginTrabajador.java
@@ -31,7 +31,6 @@ import javax.swing.event.*;
 import org.sqlite.core.DB;
 
 import gestion.VentanaGestionCliente;
-import panel.VentanaPanelAdmin;
 import panel.VentanaPanelTrabajador;
 
 public class VentanaLoginTrabajador extends JFrame {
@@ -126,14 +125,9 @@ public class VentanaLoginTrabajador extends JFrame {
 
 						@Override
 						public void actionPerformed(ActionEvent e) {
-							if(cargo.matches("administrador")){
-								new VentanaPanelAdmin();
-							}else {
-								new VentanaPanelTrabajador();
-								
-							}
-						
 							dispose();
+							new VentanaPanelTrabajador();					
+							
 
 						}
 							
